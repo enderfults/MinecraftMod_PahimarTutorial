1af0768889c3d1ca7a23722737b70e6455c42edd
enderfults <enderfults@gmail.com>
2015-07-12 12:20:52 -0500

    Basic Mod Loading + Modinfo
    Pahimar Episodes 1-4

---
+++ LetsModRebootEnder/src/main/java/com/enderfults/letsmodender/LetsModRebootEnder.java
@@ -0,0 +1,33 @@
+package com.enderfults.letsmodender;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="LetsModRebootEnder", name="Lets Mod Reboot", version="1.8-1.0")
+public class LetsModRebootEnder
+{
+	@Mod.Instance
+	public static LetsModRebootEnder instance;
+	
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+	
+	}
+	
+	
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+	
+	}
+	
+	
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+	
+	}
+}
